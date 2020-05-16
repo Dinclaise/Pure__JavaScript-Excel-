3a1b47913526d7cbeb1447c37f18151db6703f5f
Anton <aanohin98@gmail.com>
2020-05-16 20:26:54 +0300

    lesson 13

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.export = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
