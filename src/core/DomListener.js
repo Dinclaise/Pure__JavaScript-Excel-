575fb30e50f4fb2c84bea1b112b3f0f53684457f
Anton <aanohin98@gmail.com>
2020-05-27 21:13:00 +0300

    add events

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,18 @@
 export class DomListener {
-    constructor($root) {
+    constructor($root, listeners = []) {
         if (!$root) {
             throw new Error('No $root provided for DomListener!');
         }
         this.$root = $root;
+        this.listeners = listeners;
     }
+
+    initDOMListeners() {
+        // console.log(this.listeners)
+        this.listeners.forEach(listener => {
+            console.log(listener)
+        })
+    }
+
+    removeDOMListeners() {}
 }
