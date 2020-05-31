7f0a4ba58ccba7015e4239b9de99eae413b81f09
Anton <aanohin98@gmail.com>
2020-05-31 15:29:11 +0300

    added removeListeners

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -6,7 +6,7 @@ export class Formula extends ExcelComponent {
     constructor($root) {
         super($root, {
             name: 'Formula',
-            listeners: ['input']
+            listeners: ['input', 'click']
         })
     }
 
@@ -20,4 +20,6 @@ export class Formula extends ExcelComponent {
     onInput(event) {
         console.log('Formula onInput', event);
     }
+
+    onClick() {}
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,5 @@
+import { capitalize } from './utils';
+
 export class DomListener {
     constructor($root, listeners = []) {
         if (!$root) {
@@ -8,11 +10,28 @@ export class DomListener {
     }
 
     initDOMListeners() {
-        // console.log(this.listeners)
         this.listeners.forEach(listener => {
-            console.log(listener)
+            const method = getMethodName(listener);
+            if (!this[method]) {
+                const name = this.name || '';
+                throw new Error(`
+                    Method ${method} is not implemented in ${name} component!
+                `);
+            }
+            this[method] = this[method].bind(this);
+            // тоже самое, что и addEventListener
+            this.$root.on(listener, this[method]);
         })
     }
 
-    removeDOMListeners() {}
+    removeDOMListeners() {
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener);
+            this.$el.off(listener, this[method]);
+        })
+    }
+}
+
+function getMethodName(eventName) {
+    return 'on' + capitalize(eventName);
 }
