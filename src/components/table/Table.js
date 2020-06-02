b9f7ff9187d372e144d19a2f09d87314e4735568
Anton <aanohin98@gmail.com>
2020-06-02 15:34:22 +0300

    added resize for column

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -18,13 +18,20 @@ export class Table extends ExcelComponent {
     onMousedown(event) {
         // const resize = event.target.dataset.resize;
         const $resizer = $(event.target);
-        const $parent = $resizer.closest('[data-type="resizable"');
+        const $parent = $resizer.closest('[data-type="resizable"]');
+        console.log($resizer);
+
         const coords = $parent.getCoords()
 
         document.onmousemove = e => {
+            console.log('mousemove');
+
             const delta = e.pageX - coords.right;
             const value = coords.width + delta;
             $parent.$el.style.width = value + 'px';
+
+            document.querySelectorAll(`[data-col="${$parent.data.col}"]`)
+                .forEach(el => el.style.width = value + 'px')
         }
 
         document.onmouseup = e => {
